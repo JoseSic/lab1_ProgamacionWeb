426c1052e36d349bd848e7e9cf46af2aa2aeb955
JoseSic <jose.sic96@gmail.com>
2018-09-09 21:17:46 -0600

    Propuesta Proyecto

---
+++ app.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log( "ready!" );
+    var btn = document.getElementById('Aceptar');
+    btn.addEventListener('click', function() {      
+
+        var nuevoElemento = document.getElementById('oculto');
+        nuevoElemento.style.display = 'block';
+
+        console.log( "ya!" );
+    });
+
+    var btn2 = document.getElementById('B1');
+    btn2.addEventListener('click', function() {      
+
+        var nuevoElemento2 = document.getElementById('ImagenDire');
+        nuevoElemento2.style.display = 'block';
+
+        console.log( "ya!" );
+    });
+
+    var btn = document.getElementById('Editar');
+    btn.addEventListener('click', function() {      
+
+        var nuevoElemento2 = document.getElementById('ImagenDire');
+        nuevoElemento2.style.display = 'none';
+
+        console.log( "ya!" );
+    });
+    var btn = document.getElementById('Editar2');
+    btn.addEventListener('click', function() {      
+
+        var nuevoElemento2 = document.getElementById('ImagenDire');
+        nuevoElemento2.style.display = 'none';
+
+        console.log( "ya!" );
+    });
+    var btn = document.getElementById('Editar3');
+    btn.addEventListener('click', function() {      
+
+        var nuevoElemento2 = document.getElementById('ImagenDire');
+        nuevoElemento2.style.display = 'none';
+
+        console.log( "ya!" );
+    });
+    var btn = document.getElementById('Editar4');
+    btn.addEventListener('click', function() {      
+
+        var nuevoElemento2 = document.getElementById('ImagenDire');
+        nuevoElemento2.style.display = 'none';
+
+        console.log( "ya!" );
+    });
+    var btn = document.getElementById('Editar5');
+    btn.addEventListener('click', function() {      
+
+        var nuevoElemento2 = document.getElementById('ImagenDire');
+        nuevoElemento2.style.display = 'none';
+
+        console.log( "ya!" );
+    });
+
+});
